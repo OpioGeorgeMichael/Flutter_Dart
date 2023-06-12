40cc3d1bcbe7fc199933cc0ab8b5b3a2d89fcfe3
George Michael Opio <georgemichaelopio@gmail.com>
2023-06-12 12:24:40 +0300

    Create hello_world.dart

---
+++ dart_basics/hello_world.dart
@@ -0,0 +1,7 @@
+/*
+Dart program always starts with the main() function
+Inside the main() function, print() function displays the message Hello, World!
+*/
+void main() {
+  print("Hello World");
+}
